34b128da87b239329f2aff7906963ef298535dc6
ankiteshwarumale <52988396+ankiteshwarumale@users.noreply.github.com>
2019-08-03 22:50:03 +0530

    Update TestDEMOGitHubClass.java
    conflict resolves

--- SeleniumScenarios/src/main/java/Programs/TestDEMOGitHubClass.java
+++ SeleniumScenarios/src/main/java/Programs/TestDEMOGitHubClass.java
@@ -7,11 +7,10 @@ public class TestDEMOGitHubClass {
 		System.out.println("TestDEMOGitHubClass");
 		System.out.println("2nd edit");
 		System.out.println("3rd edit");
-<<<<<<< HEAD
+
 		System.out.println("Edit in TEST DEMO ECLIPSE");
-=======
 		System.out.println("4th edit");
->>>>>>> branch 'master' of https://github.com/ankiteshwarumale/TestRepository1.git
+
 	}
 
 }
