0f88db671d01a404c298489492ab26139d3696a3
dell <dell@192.168.138.1>
2019-08-03 20:27:22 +0530

    Edit code in "Create Class After First Commit" class

--- SeleniumScenarios/src/main/java/Programs/CreateClassAfterFirstCommit.java
+++ SeleniumScenarios/src/main/java/Programs/CreateClassAfterFirstCommit.java
@@ -4,7 +4,7 @@ public class CreateClassAfterFirstCommit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Edit Create Class After First Commit");
 	}
 
 }
